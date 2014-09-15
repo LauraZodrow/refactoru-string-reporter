7bf8ea7bc94226801686c2f863bbfd74df4a0bdf
Laura <laura.zodrow@gmail.com>
2014-09-15 11:55:16 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var word = prompt('Enter a word!');
+alert(word);
+alert(word.length);
+
+var res = word.charAt(2);
+alert(res);
+
+var lowerCase = word.toLowerCase();
+alert(lowerCase);
+
+var upperCase = word.toUpperCase();
+alert(upperCase);
+
+alert("Let's all go to " + word + "!");
+
+alert(word.substring(1,4)); 
